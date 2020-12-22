abe99453a512111e953b645a43f869230345c1fa
IudexSoren <aaronsibajam@gmail.com>
2020-12-21 23:48:23 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
   urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-  urlBD = 'mongodb+srv://IudexSoren:tezYdNUfeQWUC7eJ@cluster0.x0nmg.mongodb.net/cafe';
+  urlBD = process.env.MONGO_URI;
 }
 process.env.URLDB = urlBD;
